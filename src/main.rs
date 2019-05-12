d2c5b4a8d3a0b1d615efc65e4221b8d15700a2f6
fulara <ntszar@gmail.com>
2019-05-12 08:52:03 -0700

    Added lights and filters, also feeding now disabled filters for the duration of the feed.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate rppal;
 
+#[macro_use]
+extern crate structopt;
+
 use std::thread;
 use std::time::Duration;
 
 use rppal::gpio::{Gpio, Mode, Level};
 use rppal::system::DeviceInfo;
+
+use structopt::StructOpt;
 
 //# GPIO17,GPIO22,GPIO23,GPIO24
 //StepPins = [4,17,27,22]
@@ -44,7 +49,18 @@ use rppal::system::DeviceInfo;
 // The GPIO module uses BCM pin numbering. BCM GPIO 18 is tied to physical pin 12.
 // const GPIO_LED: u8 = 18;
 const STEPS_360: u32 = 4076;
-const PINS : [u8; 4] = [4,17,27,22];
+const LIGHT_PIN: u8 = 18;
+const FILTERS_PINS: [u8; 2] = [23,24];
+const FEEDER_PINS : [u8; 4] = [4,17,27,22];
+
+fn all_pins() -> Vec<u8> {
+    let mut pins = Vec::new();
+    pins.push(LIGHT_PIN);
+    pins.extend_from_slice(&FILTERS_PINS);
+    pins.extend_from_slice(&FEEDER_PINS);
+    pins
+}
+
 fn seq() ->  Vec<[u8;4]> {
     vec![
        [1,0,0,1],
@@ -97,7 +113,7 @@ fn rotate(gpio : &mut Gpio, rotation : Rotation) {
         for state in seq.iter() {
             for (index, value) in state.iter().enumerate() {
                 let state = if *value == 0 { Level::Low } else { Level::High };
-                gpio.write(PINS[index], state);
+                gpio.write(FEEDER_PINS[index], state);
             }
 
             thread::sleep(Duration::from_millis(1));
@@ -105,34 +121,74 @@ fn rotate(gpio : &mut Gpio, rotation : Rotation) {
     }
 }
 
+fn level_from_bool(b : bool) -> Level {
+    if b { Level::High } else { Level::Low }
+}
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(long = "lights")]
+    toggle_lights : Option<bool>,
+
+    #[structopt(long = "feed")]
+    feed : Option<u32>,
+
+    #[structopt(long = "filters")]
+    force_filters : Option<bool>,
+}
+
+fn toggle_filters(gpio : &mut Gpio, state : bool) {
+    for p in FILTERS_PINS.iter() {
+        gpio.write(*p, level_from_bool(state));
+    }
+}
+
 fn main() {
+    let opt = Opt::from_args();
+    println!("opt: {:?}", opt);
+
     let device_info = DeviceInfo::new().unwrap();
     println!("Model: {} (SoC: {})", device_info.model(), device_info.soc());
 
     let mut gpio = Gpio::new().unwrap();
-    for p in PINS.iter() {
+    gpio.set_clear_on_drop(false);
+    for p in all_pins().iter() {
         gpio.set_mode(*p, Mode::Output);
     }
 
     thread::sleep(Duration::from_millis(100));
 
-    for p in PINS.iter() {
-        gpio.write(*p, Level::Low);
+    if let Some(toggle_lights) = opt.toggle_lights {
+        gpio.write(LIGHT_PIN, level_from_bool(toggle_lights));
     }
 
-    thread::sleep(Duration::from_millis(100));
-
-    let args: Vec<String> = std::env::args().collect();
-    let angle = args[1].parse::<i32>().expect("couldnt parse angle.");
-
-    rotate(&mut gpio, Rotation::from_angle(angle));
-
-    for p in PINS.iter() {
-        gpio.write(*p, Level::Low);
+    if let Some(filters) = opt.force_filters {
+        toggle_filters(&mut gpio, filters);
     }
 
+    if let Some(feeder_rotation) = opt.feed {
+        for p in FEEDER_PINS.iter() {
+            gpio.write(*p, Level::Low);
+        }
+
+        thread::sleep(Duration::from_millis(100));
+
+        toggle_filters(&mut gpio, false);
+        thread::sleep(Duration::from_secs(90));
+
+        let rotation = feeder_rotation as i32;
+        rotate(&mut gpio, Rotation::from_angle(rotation));
+        rotate(&mut gpio, Rotation::from_angle(-rotation));
+
+        for p in FEEDER_PINS.iter() {
+            gpio.write(*p, Level::Low);
+        }
+
+        thread::sleep(Duration::from_secs(90));
+        toggle_filters(&mut gpio, true);
+    }
+    thread::sleep(Duration::from_millis(10000));
 }
-
 
 #[cfg(test)]
 mod tests {
